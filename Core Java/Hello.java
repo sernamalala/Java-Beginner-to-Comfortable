1d40900fb99f38a98b05ce837798c35fbac80552
sernamalala <sernaprograms@gmail.com>
2024-01-24 18:22:00 +0200

    initial Java programs

---
+++ Core Java/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Hello
+ */
+
+ //javac Hello.java
+ //Write once run anyway
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Serna!");
+
+        int age = 20;
+        String name = "Serna";
+        String program = "Computer Engineering";
+
+        System.out.println("Hi!\nMy name is "+name+" and I am "+age+" years old.\nI am studying "+program);
+    }
+}
